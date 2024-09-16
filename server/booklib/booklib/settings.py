ac04166ea32e100bb51be9f6f71c55d2fc6e35b8
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-09-16 19:41:44 +0530

    Update settings.py

--- server/booklib/booklib/settings.py
+++ server/booklib/booklib/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'booklib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'book_library',
+        'USER': 'root',
+        'PASSWORD' : '2511',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
@@ -129,3 +133,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+# Define the media root and URL for serving media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
