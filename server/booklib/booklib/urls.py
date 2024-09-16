c2301ab9f20f63d911d06167e72153b0ec1777f4
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-09-16 19:41:47 +0530

    Update urls.py

--- server/booklib/booklib/urls.py
+++ server/booklib/booklib/urls.py
@@ -1,8 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("books.urls")),
     path('admin/', admin.site.urls),
+    path('', include('books.urls')),  # Include URLs from your app
+    # Add other URL patterns here
 ]
+
+# Add this only during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
