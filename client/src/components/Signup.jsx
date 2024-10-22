ea3a440d6514f1decf9b83bc58f33eb9c1fc2684
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-10-22 20:35:02 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Signup.css';
 import signupImage from '../assets/login-image.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const Signup = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="signup-page">
       <div className="signup-container">
+        {/* Home button */}
+        <button onClick={() => navigate('/')} className="home-button">
+        <FontAwesomeIcon icon={faHouse} />
+        </button>
+
         {/* Left side for the image */}
         <div className="signup-image">
           <img src={signupImage} alt="Signup" />
