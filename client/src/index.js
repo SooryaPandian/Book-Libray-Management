78bd2e1b227b34736080961b0515c481b77fa50a
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-11-08 22:42:45 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
+import { ThemeProvider } from './components/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
